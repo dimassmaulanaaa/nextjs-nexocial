65684ccb9920c8502113bcdb05f3b283c5c8b5e2
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-07-19 09:32:50 +0700

    feat: initialize prisma

---
+++ src/actions/user.action.ts
@@ -0,0 +1,39 @@
+"use server";
+
+import { auth, currentUser } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+export async function syncUser() {
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+
+    if (!userId || !user) {
+      throw new Error("User not authenticated");
+    }
+
+    const dbUser = await prisma.user.upsert({
+      where: {
+        clerkId: userId,
+      },
+      update: {
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        name: `${user.firstName || ""} ${user.lastName || ""}`.trim(),
+        image: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+      create: {
+        clerkId: userId,
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        name: `${user.firstName || ""} ${user.lastName || ""}`.trim(),
+        image: user.imageUrl,
+      },
+    });
+
+    return dbUser;
+  } catch (error) {
+    console.error("ERROR in syncUser:", error);
+    throw new Error("Failed to sync user with the database");
+  }
+}
