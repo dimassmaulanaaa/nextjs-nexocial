8f2330e8c11e03b787efd57d0bcfa3fdf9a6578d
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-07-29 22:38:22 +0700

    refactor(actions): remove unused getUserByClerkId function

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -36,23 +36,6 @@ export async function syncUser() {
   }
 }
 
-export async function getUserByClerkId(clerkId: string) {
-  return prisma.user.findUnique({
-    where: {
-      clerkId,
-    },
-    include: {
-      _count: {
-        select: {
-          followers: true,
-          following: true,
-          posts: true,
-        },
-      },
-    },
-  });
-}
-
 export async function getCurrentUserId() {
   const { userId: clerkId } = await auth();
 
