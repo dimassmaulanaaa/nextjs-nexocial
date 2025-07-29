48456281e2c8364729932ed321598d16d25e7ada
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-07-29 15:49:08 +0700

    feat(utils): add toTitleCase helper function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function toTitleCase(text: string) {
+  if (!text || text.length === 0) {
+    return "";
+  }
+  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
 }
