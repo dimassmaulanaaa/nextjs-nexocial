a87e06423b0c0b234c4b1ea402a9d4ab9f9898f5
Dimas Maulana <dimasmaulana.idn@gmail.com>
2025-08-14 08:39:59 +0700

    refactor(utils): remove unused toTitleCase function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,10 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function toTitleCase(text: string) {
-  if (!text || text.length === 0) {
-    return "";
-  }
-  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
-}
